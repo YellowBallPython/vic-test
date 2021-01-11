5bb340fb98b937d1d448cf111556d233226b8ce3
YellowBallPython <yellowballpython@gmail.com>
2021-01-11 15:18:41 -0600

    Frontend app_name updated
    Se actualuza el nombre del app_name a 'frontend', prior was 'front'

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'front'
+app_name = 'frontend'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.index, name='index'),
