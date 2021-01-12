f28017ee6a08418f8f5eed376e6abe5ed8ba0f44
YellowBallPython <yellowballpython@gmail.com>
2021-01-11 22:52:42 -0600

    Add Static MEDIA ROOT/URL
    Agregadas al urls.py principal

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,9 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     pic = models.ImageField(verbose_name='profile pic',
-                            upload_to='profile_pics')
+                            upload_to='profile_pics',
+                            default='default-profile.jpg'
+                            )
 
     def __str__(self):
         return f'{self.user} Profile'
